5ec71cf1c985545da2d6cf79dafb2ab8f2a88835
Michael <michaelx.10@hotmail.com>
2025-03-23 14:29:20 -0400

    reset trace due to misunderstanding

--- src/trace.rs
+++ src/trace.rs
@@ -3,5 +3,20 @@ use crate::cpu::Mem;
 use crate::cpu::CPU;
 use std::collections::HashMap;
 
-// This will print out the current status of the CPU in the format of
-pub fn trace(cpu: &CPU) -> String {}
+// This will return the current state of the cpu based on its parameters in trace
+pub fn trace(cpu: &CPU) -> String {
+    let code = cpu.instr;
+    let pc = cpu.pc - cpu.cycles as u16; // Artifically pulling to the start of the command(PC is on the )
+    let mode = cpu.mode;
+
+    let mut hex_dump = vec![];
+
+     let (mem_addr, stored_value) = match mode {
+        AddressingMode::Immediate | AddressingMode::NoneAddressing => (0, 0),
+        _ => {
+            let addr = cpu.get_absolute_address(&ops.mode, begin + 1);
+            (addr, cpu.mem_read(addr))
+        }
+    };
+
+}
